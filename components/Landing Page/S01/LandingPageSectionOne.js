431b61b598de44ca537454121d611cc9fafaa96d
Mayo <makabmahlalela@gmail.com>
2024-10-04 16:15:32 +0200

    Update LandingPageSectionOne.js

--- components/Landing Page/S01/LandingPageSectionOne.js
+++ components/Landing Page/S01/LandingPageSectionOne.js
@@ -6,7 +6,7 @@ import Video from './Video'
 export default function LandingPageSectionOne() {
   return (
     <div>
-        <Image src={LP} alt="section-one" width={1920} height={1080} />
+        <Image src={LP} alt="section-one" width={1020} height={1080} />
 
     </div>
   )
